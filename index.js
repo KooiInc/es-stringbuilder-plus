b778f870eac50af5fd4b0d6967edfaeba5161b6c
KooiInc <renzo.kooi@gmail.com>
2023-12-26 12:13:43 +0100

    update interpolator

--- index.js
+++ index.js
@@ -289,25 +289,24 @@ function descriptionsGetter() {
     }
 }
 
-function interpolateFactory() {
-  const {isStringOrNumber, isObject, invalidate, replacement, replacer, replace} = {
-    isStringOrNumber: v => [String, Number].find(vv => vv === Object.getPrototypeOf( v ?? {} )?.constructor),
-    isObject: v => Object.getPrototypeOf( v ?? `` )?.constructor === Object,
-    invalidate: (defaultReplacer, key) => defaultReplacer ?? `{${key}}`,
-    replacement: (key, t, defaultReplacer) =>
-      !isStringOrNumber(t[key]) || (!defaultReplacer && `${t[key]}`.trim() === ``)
-        ? invalidate(defaultReplacer, key) : t[key] ?? invalidate(defaultReplacer, key),
-    replacer: (token, defaultReplacer) => (...args) =>
-      replacement( args.find(a => a.key).key ?? `_`, token, defaultReplacer ),
-    replace: (str, token, defaultReplacer) =>
-      str.replace( /\{(?<key>[a-z_\d]+)}/gim, replacer(token, defaultReplacer) ),
-  };
-  const interpolate = (str, defaultReplacer, ...tokens) => tokens.flat()
-    .reduce( (acc, token) =>
-      acc.concat(!isObject(token) ? `` : replace(str, token, defaultReplacer )), ``);
+function interpolateFactory(defaultReplacer) {
+  const isStringOrNumber = v => [String, Number].find(vv => vv === Object.getPrototypeOf( v ?? {} )?.constructor);
+  const isObject = v => Object.getPrototypeOf( v ?? `` )?.constructor === Object;
+  const invalidate = key => String(defaultReplacer ?? `{${key}}`);
+  const replacement = (key, t) => !isStringOrNumber(t[key]) ? invalidate(key) : String(t[key]);
+  const replacer = token => (...args) => replacement( args.find(a => a.key).key ?? `_`, token );
+  const replace = (str, token) => str.replace( /\{(?<key>[a-z_\d]+)}/gim, replacer(token) );
+  const mergeToken = obj => {
+    const entries = Object.entries(obj);
+    const [key, values] = entries.shift();
+    const merged = values.reduce( (acc, v) => [...acc, {[key]: v}], []);
+    entries.forEach( ([key, value]) => value.forEach( (v, i) =>  merged[i][key] = `${v}`));
+    return merged;  };
+  const isMultiLineWithArrays = (...tokens) =>
+    tokens.length === 1 && Object.values(tokens[0]).filter(v => !Array.isArray(v)).length < 1;
+  const processTokens = (...tokens) => isMultiLineWithArrays(...tokens) ? mergeToken(tokens.shift()) : tokens;
+  const interpolate = (str, ...tokens) => tokens.flat()
+    .reduce( (acc, token) => acc.concat(!isObject(token) ? `` : replace(str, token )), ``);
   
-  return (str, ...tokens) => {
-    const defaultReplacer = isStringOrNumber(tokens[0]) ? String(tokens.shift()) : undefined;
-    return interpolate(str, defaultReplacer, ...tokens)
-  }
+  return (str, ...tokens) => interpolate(str, ...processTokens(...tokens));
 }
